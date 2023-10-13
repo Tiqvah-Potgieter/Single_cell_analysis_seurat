cb8c93ce30f5061dbb4e206735383b25bbe8f49d
Tiqvah-Potgieter <tpotgieter@sun.ac.za>
2023-10-13 11:41:21 +0200

    first rserver commit

---
+++ First-sample1,7.R
@@ -0,0 +1,14 @@
+# download libraries 
+install.packages("Seurat")
+install.packages("dplyr")
+install.packages("patchwork")
+install.packages("ggplot2")
+
+# load libraries
+library(Seurat)
+library(dplyr)
+library(patchwork)
+library(ggplot2)
+library(tidyverse)
+
+convert
